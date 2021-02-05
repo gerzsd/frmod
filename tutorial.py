835f61475d513d5a5246b52dd0ee5acdf8d49a2b
gerzsd <50586392+gerzsd@users.noreply.github.com>
2021-02-05 16:26:48 +0100

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -32,3 +32,15 @@ if __name__ == "__main__":
     # Plotting the success rate curve
     success_rates = fra.get_src()
     fra.plot_success_rates()
+    slope_df1 = fra.fr_stats_full["slope"][0]
+    slope_df2 = fra.fr_stats_full["slope"][1]
+    
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.set_xlabel("Distribution of slope values in the LS and NLS areas")
+    ax2.set_xlabel("Frequency ratio [LS / NLS]")
+    line_LS, = ax1.plot(slope_df1["min"], slope_df1["LS_density"])
+    line_NLS, = ax1.plot(slope_df1["min"], slope_df1["NLS_density"])
+    line_fr =ax2.plot(slope_df1["min"], slope_df1["frequency_ratio"])
+    plt.tight_layout()
+    plt.show()
+
