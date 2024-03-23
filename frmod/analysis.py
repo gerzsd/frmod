818807a8eeca2c3dbf5eedc963ec58a8d4af5c68
Dávid Gerzsenyi <gerzsd@caesar.elte.hu>
2024-03-23 20:53:45 +0100

    Possibility to get_result with product=True/False
    product=False: averaged susceptibility grids
    product=True: product of the analysed variable susceptibility grids makes the final susceptibility grid

--- frmod/analysis.py
+++ frmod/analysis.py
@@ -616,7 +616,7 @@ class FRAnalysis():
         self.stats[vrr.name] = stat_df
         return rc_folds
 
-    def get_result(self):
+    def get_result(self, product=False):
         """
         Get the susceptibility estimation and ranks.
 
@@ -635,12 +635,18 @@ class FRAnalysis():
         percentile_bins = [x * 0.01 for x in range(0, 101)]
         # Iterate over the folds of the ls_mask.
         for i in range(0, self.ls_mask.fold_count):
-            fold_result = np.zeros(rc_folds[0][0].shape)
-            
+            if product:
+                fold_result = np.ones(rc_folds[0][0].shape)
+            else:
+                fold_result = np.zeros(rc_folds[0][0].shape)
+
             # Iterate over the reclassified grids of fold #i
             for j in rc_folds:
-                # Average the reclassified  grids of fold #i.
-                fold_result += j[i] / self.var_count
+                if product:
+                    fold_result *= j[i]
+                else:
+                    # Average the reclassified  grids of fold #i.
+                    fold_result += j[i] / self.var_count
 
             # Percentile bin edges for conversion to percentiles
             valid_perc = np.quantile(fold_result[fold_result >= 0],
@@ -671,7 +677,7 @@ class FRAnalysis():
             fp = reclass_raster(result[i],
                                 percentile_bins[1:],
                                 self.valid_perc[i])
-            fp[fp > 0] = fp[fp > 0] * 100
+            fp[fp >= 0] = fp[fp >= 0] * 100
             self.fold_percentiles.append(fp)
 
         # Set all invalid values (<0) to -99999 (nodata)
@@ -679,7 +685,7 @@ class FRAnalysis():
 
         self.fold_susceptibility = result
         self.fresult = fresult
-        self.ranks = np.quantile(fresult[fresult > 0], percentile_bins)
+        self.ranks = np.quantile(fresult[fresult >= 0], percentile_bins)
         return self.ranks
 
     def get_src(self):
